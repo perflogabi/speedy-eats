143760ecbd0d7474313ae157d66f8bfcfee2e962
Gabriela Florêncio <perflogabi@gmail.com>
2024-05-02 11:04:27 -0300

    style: add overflow-x-scroll

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./category-item";
 const CategoryList = async () => {
   const categories = await db.category.findMany({});
   return (
-    <div className="flex gap-3 overflow-scroll">
+    <div className="flex gap-3 overflow-x-scroll">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
