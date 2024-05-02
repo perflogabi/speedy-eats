7b43c7caaa73ce34d568f057153e41c277ed6532
Gabriela Florêncio <perflogabi@gmail.com>
2024-05-01 23:42:14 -0300

    feat: add initial search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logospeed.jpg" alt="Speed Eats" height={20} width={60} />
+      <Image src="/logospeed.jpg" alt="Speed Eats" height={20} width={160} />
       <Button
         size="icon"
         variant="outline"
